57acfefbc12d84b38e7930c24e7fcf7094a9a805
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-11-27 23:17:02 +0100

    Switch to master channel for now

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-FLUTTER_BRANCH=`grep channel: .metadata | sed 's/  channel: //g'`
-
 # Get flutter
 git clone https://github.com/flutter/flutter.git
 FLUTTER=flutter/bin/flutter
 
 #Flutter channel cmd
-cmd="${FLUTTER} channel ${FLUTTER_BRANCH}"
+cmd="${FLUTTER} channel master"
 
 # Set channel info and update
 DIR=$($cmd >& /dev/stdout)
