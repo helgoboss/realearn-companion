bd2f6dc763e1de6f7c2d7f54713af15ca972c44e
Benjamin Klum <benjamin.klum@helgoboss.org>
2021-03-18 12:13:18 +0100

    Make main arg list optional for web target

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,6 @@ import 'infrastructure/platform/stub/configure_app.dart'
     if (dart.library.html) 'infrastructure/platform/web/configure_app.dart'
     if (dart.library.io) 'infrastructure/platform/native/configure_app.dart';
 
-void main(List<String> arguments) {
+void main([List<String> arguments]) {
   App.run(config: configureApp(arguments));
 }
